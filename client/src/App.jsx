cb95fcdf119ae0fd292e32a718bfdbbdcd8fa656
ayomide adeniyi <addysam@yahoo.com>
2026-09-11 13:10:48 +0100

    Setup MERN project structure and MongoDB connection

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div>
+      <h1>Propify Property Listing</h1>
+      <p>MERN Stack Real Estate Application</p>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+
+const app = express();
+
+
+connectDB();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Propify API is running");
+});
+
+
+// Server port
+const PORT = process.env.PORT || 5000;
+
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
